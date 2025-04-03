850ae3d0e51459d9771c58924501fcfe34fa2590
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-04-02 23:06:37 -0400

    add click

--- Drum+Kit+Starting+Files/DrumKitStartingFiles/index.js
+++ Drum+Kit+Starting+Files/DrumKitStartingFiles/index.js
@@ -1,3 +1,8 @@
-function handleClick() {
-    alert("I go clicked!");
-}
+var numberOfButtons = document.querySelectorAll(".drum").length; //pegando o número de botões
+
+for (var i = 0; i<numberOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I go clicked!");
+});
+    } 
+    
