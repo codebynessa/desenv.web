1780d17c36ebb07fffd19632ed25f46da7ea6c53
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-04-05 17:12:24 -0400

    add animacao

--- Drum+Kit+Starting+Files/DrumKitStartingFiles/index.js
+++ Drum+Kit+Starting+Files/DrumKitStartingFiles/index.js
@@ -1,23 +1,22 @@
+// pegando o numero de botoes com a classe "drum"
+var drumButtons = document.querySelectorAll(".drum");
 
-//detectando o clique do mouse
-var numberOfButtons = document.querySelectorAll(".drum").length; //pegando o número de botões
-
-for (var i = 0; i<numberOfButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-var buttonInnerHTML = this.innerHTML;
-makeSound(buttonInnerHTML);
-
+for (var i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML); // toca o som
+        buttonAnimation(buttonInnerHTML); // faz a animacao
     });
-
 }
-//detectando o pressionar de uma tecla
 
-document.addEventListener("keydown", function(event){
-    makeSound(event.key);
+// detectando o pressionar de uma tecla
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key); // toca o som
+    buttonAnimation(event.key); // faz a animacao
 });
 
+// funcao que toca o som correspondente a tecla
 function makeSound(key) {
-    //switch case para tocar o som correto
     switch (key) {
         case "w":
             var crash = new Audio("./sounds/crash.mp3");
@@ -34,7 +33,7 @@ function makeSound(key) {
             snare.play();
             break;
 
-        case "d":  
+        case "d":
             var tom1 = new Audio("./sounds/tom-1.mp3");
             tom1.play();
             break;
@@ -54,47 +53,20 @@ function makeSound(key) {
             tom4.play();
             break;
 
-        default: console.log(buttonInnerHTML);
+        default:
+             alert("tecla invalida: " + key);
     }
 }
-//detectando o clique do mouse
-switch (buttonInnerHTML) {
-    case "w":
-        var crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
 
-    case "a":
-        var kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
-        break;
+// funcao para animacao do botao pressionado
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
 
-    case "s":
-        var snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
+    if (activeButton) {
+        activeButton.classList.add("pressed"); // adiciona a classe "pressed"
 
-    case "d":  
-        var tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-    case "j":
-        var tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-    case "k":
-        var tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-    case "l":
-        var tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-    default: console.log(buttonInnerHTML);
+        setTimeout(function () {
+            activeButton.classList.remove("pressed"); // remove a classe depois de 100ms
+        }, 100);
+    }
 }
- 
-    
