36f54fd7df517e1462aa7a43a2b13cbbf922e53f
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-04-05 16:32:33 -0400

    Adicionando nova pasta

--- Drum+Kit+Starting+Files/DrumKitStartingFiles/index.js
+++ Drum+Kit+Starting+Files/DrumKitStartingFiles/index.js
@@ -1,9 +1,63 @@
+
+//detectando o clique do mouse
 var numberOfButtons = document.querySelectorAll(".drum").length; //pegando o número de botões
 
 for (var i = 0; i<numberOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 var buttonInnerHTML = this.innerHTML;
+makeSound(buttonInnerHTML);
 
+    });
+
+}
+//detectando o pressionar de uma tecla
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    //switch case para tocar o som correto
+    switch (key) {
+        case "w":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "d":  
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+    }
+}
+//detectando o clique do mouse
 switch (buttonInnerHTML) {
     case "w":
         var crash = new Audio("./sounds/crash.mp3");
@@ -42,8 +96,5 @@ switch (buttonInnerHTML) {
 
     default: console.log(buttonInnerHTML);
 }
-    }
-    );  
-}
-    
+ 
     
