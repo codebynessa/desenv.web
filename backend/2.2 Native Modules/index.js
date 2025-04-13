227dae5277d0c2f676ae07452c28d1aa6b9d6f75
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-04-13 16:47:10 -0400

    add npm

--- backend/2.2 Native Modules/index.js
+++ backend/2.2 Native Modules/index.js
@@ -1,6 +1,11 @@
-const fs = require("fs");
+const fs = require("fs"); //faz parte do core do node
 
-fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-});
+// fs.writeFile("message.txt", "Hello from NodeJS!", (err) => { // o primeiro argumento é o nome do arquivo, o segundo é o conteudo
+//     if (err) throw err; // se der erro, lança o erro
+//     console.log("The file has been saved!"); // se der certo, imprime a mensagem
+// });
+
+fs.readFile("./message.txt", "utf8", (err, data) => { // o primeiro argumento é o nome do arquivo, o segundo é o encoding
+    if (err) throw err; // se der erro, lança o erro
+    console.log(data); // se der certo, imprime o conteudo do arquivo
+    });
