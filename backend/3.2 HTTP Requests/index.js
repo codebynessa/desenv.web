345688dbeb0dc4c4e4d79de00fd52dc6ce24a301
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-04-15 23:19:10 -0400

    postman

--- backend/3.2 HTTP Requests/index.js
+++ backend/3.2 HTTP Requests/index.js
@@ -1,9 +1,9 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express from "express"; // importa express
+const app = express(); //cria uma instancia do express
+const port = 3000; // define a porta do servidor
 
-app.get("/", (req, res) => {
-  res.send("<h1>Helloo</h1>");
+app.get("/", (req, res) => { // rota raiz
+  res.send("<h1>Helloo</h1>"); // envia resposta para o cliente
 });
 
 app.get("/about", (req, res) => {
@@ -14,6 +14,6 @@ app.get("/contact", (req, res) => {
   res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(port, () => { // inicia o servidor na porta 3000
+  console.log(`Server started on port ${port}`); // imprime mensagem de sucesso
 });
